88091c943f333a543cf704c0c34ad8b0aca3148c
Jean Brisou <jean.brisou@kedgebs.com>
2020-05-28 17:01:20 +0200

    ex2

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+  def new
+    # we need @cocktail in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `cocktail_id` to associate dose with corresponding cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts "cleaning DB"
+Ingredient.destroy_all
+
+# puts "Creating ingredients..."
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  attribute = ingredient["strIngredient1"]
+  new_ingredient = Ingredient.create(name: attribute)
+  puts "#{new_ingredient.name} has been created"
+end
